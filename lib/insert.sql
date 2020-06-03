55880300bb2c2724c6aae886ed2cbce64154d9be
Matt Im <matthew.seiim@gmail.com>
2020-06-03 05:10:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,2 @@
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES
-(1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
