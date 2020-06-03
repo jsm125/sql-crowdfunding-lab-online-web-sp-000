864f5a67b83765e8e6f99e93ef0e10be9dbdb021
Matt Im <matthew.seiim@gmail.com>
2020-06-03 05:12:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,7 @@ VALUES (1,'Master_bedroom','Home_improvement',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
 VALUES (1,'Master_bathroom','Home_improvement',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+VALUES (1,'Den','Home_improvement',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
 VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
