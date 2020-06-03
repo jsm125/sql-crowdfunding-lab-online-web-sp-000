2584a3b28616df326b6ffebd70dcfed9f4d034ba
Matt Im <matthew.seiim@gmail.com>
2020-06-03 05:27:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,41 +1,41 @@
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
 VALUES (1,'Kitchen','Home_improvement',5000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Living Room','Home_improvement',15000,2020-02-22,2020-02-28);
+VALUES (2,'Living Room','Home_improvement',15000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Roof','Home_improvement',10000,2020-02-22,2020-02-28);
+VALUES (3,'Roof','Home_improvement',10000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Master_bedroom','Home_improvement',23000,2020-02-22,2020-02-28);
+VALUES (4,'Master_bedroom','Home_improvement',23000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Master_bathroom','Home_improvement',20000,2020-02-22,2020-02-28);
+VALUES (5,'Master_bathroom','Home_improvement',20000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Den','Home_improvement',15000,2020-02-22,2020-02-28);
+VALUES (6,'Den','Home_improvement',15000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Siding','Home_improvement',25000,2020-02-22,2020-02-28);
+VALUES (7,'Siding','Home_improvement',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Car','Automobile',25000,2020-02-22,2020-02-28);
+VALUES (8,'Car','Automobile',25000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Computer','Electronic',2500,2020-02-22,2020-02-28);
+VALUES (9,'Computer','Electronic',2500,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Attic','Home_improvement',5000,2020-02-22,2020-02-28);
+VALUES (10,'Attic','Home_improvement',5000,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Speakers','Audio_Equipment',270,2020-02-22,2020-02-28);
+VALUES (11,'Speakers','Audio_Equipment',270,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Sneakers','Clothing',500,2020-02-22,2020-02-28);
+VALUES (12,'Sneakers','Clothing',500,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Wardrobe','Clothing',2500,2020-02-22,2020-02-28);
+VALUES (13,'Wardrobe','Clothing',2500,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Headphones','Audio_Equipment',350,2020-02-22,2020-02-28);
+VALUES (14,'Headphones','Audio_Equipment',350,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Furniture','Home_improvement',2500,2020-02-22,2020-02-28);
+VALUES (15,'Furniture','Home_improvement',2500,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Washer','Home_improvement',150,2020-02-22,2020-02-28);
+VALUES (16,'Washer','Home_improvement',150,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Dryer','Home_improvement',250,2020-02-22,2020-02-28);
+VALUES (17,'Dryer','Home_improvement',250,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Dishwasher','Home_improvement',250,2020-02-22,2020-02-28);
+VALUES (18,'Dishwasher','Home_improvement',250,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Air Conditioner','Home_improvement',250,2020-02-22,2020-02-28);
+VALUES (19,'Air Conditioner','Home_improvement',250,2020-02-22,2020-02-28);
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
-VALUES (1,'Patio','Home_improvement',25000,2020-02-22,2020-02-28);
+VALUES (20,'Patio','Home_improvement',25000,2020-02-22,2020-02-28);
 
