0e82c902ef9109ca2b5bcab71400d3b1fd87dcb1
Matt Im <matthew.seiim@gmail.com>
2020-06-03 05:04:27 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,17 +5,17 @@ category TEXT,
 funding_goal INTEGER,
 start_date INTEGER,
 end_date INTEGER
-)
+);
 
 CREATE TABLE users(
 id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER
-)
+);
 
 CREATE TABLE pledges(
 id INTEGER PRIMARY KEY,
 amount INTEGER,
 user_id TEXT,
 project_id TEXT
-)
+);
