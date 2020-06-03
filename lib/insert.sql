9e6dab97d1a38d9f6e9046f8f6f8df3938ccff21
Matt Im <matthew.seiim@gmail.com>
2020-06-03 05:12:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,41 @@
 INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
 VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Living Room','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Roof','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Master_bedroom','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Master_bathroom','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+INSERT INTO projects(id,title,category,funding_goal,start_date,end_date)
+VALUES (1,'Kitchen','Home_improvement',25000,2020-02-22,2020-02-28);
+
