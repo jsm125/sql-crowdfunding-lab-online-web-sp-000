3acebd611118c46178fb2c9ce27ff71376bb0c28
Matt Im <matthew.seiim@gmail.com>
2020-06-03 17:21:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   ON pledges.project_id = projects.id 
   GROUP BY title 
   HAVING SUM(amount) 
-  ORDER BY title"
+  ORDER BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -23,7 +23,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   ON pledges.user_id = users.id
   GROUP BY name
   HAVING SUM(amount)
-  ORDER BY name ASC"
+  ORDER BY name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -32,7 +32,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY title
-  HAVING SUM(amount) >= funding_goal"
+  HAVING SUM(amount) >= funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
@@ -41,7 +41,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY name
-  ORDER BY SUM(amount)"
+  ORDER BY SUM(amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
@@ -49,7 +49,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   JOIN pledges
   ON  projects.id = pledges.user_id
-  WHERE category = 'music'"
+  WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
